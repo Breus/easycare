b6a5de370638aa68a45b9a06136096e6d48d92de
Breus <b,blaauwendraad@hotmail.nl>
2016-03-10 12:16:24 +0100

    Index html5 set-up

--- wp-content/themes/easycare/index.php
+++ wp-content/themes/easycare/index.php
@@ -1,1 +1,11 @@
 <?php /* Template Name: EasyCare index */ ?>	 
+<!doctype html>
+<html lang="en">
+		<head>
+				<meta charset="UTF-8">
+				<title>Document</title>
+		</head>
+		<body>
+
+		</body>
+</html>
